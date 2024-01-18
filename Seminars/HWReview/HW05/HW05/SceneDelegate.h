d5bd1b6733d224619432c633fd00deb4b70a7fa2
Andrey <yeah-men@mail.ru>
2024-01-18 18:14:08 +0200

    Added homework for 5th seminar

---
+++ Seminars/HWReview/HW05/HW05/SceneDelegate.h
@@ -0,0 +1,14 @@
+//
+//  SceneDelegate.h
+//  HW05
+//
+//  Created by Andrey Zavershinskiy on 17.01.2024.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
