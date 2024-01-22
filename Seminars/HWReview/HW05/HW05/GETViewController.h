aa2a88ec72a432ca0270de207b1e3edbb5907a98
Andrey <yeah-men@mail.ru>
2024-01-22 16:48:16 +0200

    Fix HW05
    Restored: GETViewController & Loader

---
+++ Seminars/HWReview/HW05/HW05/GETViewController.h
@@ -0,0 +1,15 @@
+//
+//  GETViewController.h
+//  HW05
+//
+//  Created by Andrey Zavershinskiy on 22.01.2024.
+//
+
+#import <UIKit/UIKit.h>
+#import "Loader.h"
+
+@interface GETViewController : UIViewController
+
+- (instancetype)initWithLoader: (Loader *) loader;
+
+@end
