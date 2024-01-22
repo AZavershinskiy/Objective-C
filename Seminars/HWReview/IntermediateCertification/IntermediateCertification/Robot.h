fb8b2a3b13c948451a426c0283783f808d282165
Andrey <yeah-men@mail.ru>
2024-01-22 18:38:11 +0200

    Added Intermediate Certification
    Homework for 6th homework

---
+++ Seminars/HWReview/IntermediateCertification/IntermediateCertification/Robot.h
@@ -0,0 +1,23 @@
+//
+//  Robot.h
+//  IntermediateCertification
+//
+//  Created by Andrey Zavershinskiy on 22.01.2024.
+//
+
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Robot : NSObject<NSCoding, NSSecureCoding>
+
+@property (nonatomic, readonly) double xCoordinate;
+@property (nonatomic, readonly) double yCoordinate;
+@property (nonatomic, readonly) NSString *name;
+
+- (instancetype)initWithName: (NSString *)name xCoordinate: (double)x yCoordinate: (double)y;
+
+@end
+
+NS_ASSUME_NONNULL_END
